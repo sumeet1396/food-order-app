581f6e60fdfdf445ee266f7c5b911351d2dcfcaa
rsumeetprdxn <sumeetr@prdxn.com>
2021-05-30 00:16:43 +0530

    add to cart button added

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import Card from '../UI/Card';
+import Card from '../UI/Card/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './Meals.module.scss';
 
@@ -31,12 +31,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
+      <MealItem
+        id={meal.id}
         key={meal.id}
         name={meal.name}
         description={meal.description}
         price={meal.price}
-    />
+      />
     ));
 
     return (
